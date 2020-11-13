3fe2e30b0c05a7497ef747881501fdb4959e910a
kaspar suursalu <kaspar.suursalu@tptlive.ee>
2020-11-13 14:12:55 +0200

    class toggle example(red green)

--- Example.vue
+++ Example.vue
@@ -1,53 +1,26 @@
 <template>
   <div>
-    <input type="text" v-model="message" @keyup.enter="add">
-    <button @click="add">Add Item</button>
-    <check-list :items="items" title="All Items"></check-list>
-    <check-list :items="doneItems" title="Done Items"></check-list>
-    <check-list :items="notDoneItems" title="Not Done Items"></check-list>
+      <button :class="{green: isOn, red: !isOn}" @click="isOn=!isOn">Click me</button>
   </div>
 </template>
 
 <script>
-import CheckList from "./CheckList";
 export default {
   name: "Example",
-  components: {CheckList},
+
   data() {
     return {
-      message: '',
-      items: [
-        {name:'Sai', done: true, key: 0},
-        {name:'Leib', done: false, key: 1},
-        {name:'Õlu', done: true, key: 2},
-      ]
+      isOn: true
     }
   },
-  methods: {
-    add(){
-      if(this.message !== '') {
-        this.items.push({
-          name: this.message,
-          done: false,
-          key: this.items.length
-        });
-        this.message = '';
-      }
-    }
-  },
-  computed: {
-    doneItems(){
-      return this.items.filter(item => item.done);
-    },
-    notDoneItems(){
-      return this.items.filter(item => !item.done);
-    }
-  }
 }
 </script>
 
 <style scoped>
-  h1 {
-    color: red
+  .red {
+    background-color: red;
+  }
+  .green {
+    background-color: green;
   }
 </style>
