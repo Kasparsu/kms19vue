cb87f1f2e5474718924c2e9c4706157d08b83c2e
kaspar suursalu <kaspar.suursalu@tptlive.ee>
2020-11-13 12:32:10 +0200

    init commit

---
+++ Example.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <input type="text" v-model="message" @keyup.enter="items.push(message)">
+    <button @click="items.push(message)" >Add Item</button>
+    <ul>
+      <li v-for="(item,key) in items" :key="key">{{item}}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Example",
+  data() {
+    return {
+      message: '',
+      items: ['Sai', 'Piim', 'Õlu']
+    }
+  }
+}
+</script>
+
+<style scoped>
+  h1 {
+    color: red
+  }
+</style>
