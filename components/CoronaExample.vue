e7e8da0674939570798b76eecfc41d014c794a43
kaspar suursalu <kaspar.suursalu@tptlive.ee>
2020-12-03 15:21:45 +0200

    show list of countries

--- components/CoronaExample.vue
+++ components/CoronaExample.vue
@@ -1,23 +1,28 @@
 <template>
-  <ul v-if="global">
-    <li>{{global.TotalConfirmed}}</li>
-    <li>{{global.NewConfirmed}}</li>
-  </ul>
+  <div class="columns is-multiline">
+    <div class="column is-one-quarter" v-for="(country, index) in countries" :key="index">
+        <country-data :data="country"></country-data>
+    </div>
+  </div>
 </template>
 
 <script>
 import axios from 'axios';
+import CountryData from "./CountryData";
 export default {
   name: "CoronaExample",
+  components: {CountryData},
   created(){
     axios.get('https://api.covid19api.com/summary').then((resp)=>{
       console.log(resp.data);
       this.global = resp.data.Global;
+      this.countries = resp.data.Countries;
     })
   },
   data(){
     return {
-      global: null
+      global: null,
+      countries: []
     }
   }
 }
---
+++ components/CountryData.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="card">
+    <header class="card-header has-background-primary">
+      <p class="card-header-title">
+        {{data.Country}}
+      </p>
+    </header>
+    <div class="card-content">
+      <ul>
+        <li><b>New Confirmed:</b> {{data.NewConfirmed}}</li>
+        <li><b>Total Confirmed:</b> {{data.TotalConfirmed}}</li>
+        <li><b>New Deaths:</b> {{data.NewDeaths}}</li>
+        <li><b>Total Deaths:</b> {{data.TotalDeaths}}</li>
+        <li><b>New Recovered:</b> {{data.NewRecovered}}</li>
+        <li><b>Total Recovered:</b> {{data.TotalRecovered}}</li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+name: "CountryData",
+  props: ['data']
+}
+</script>
+
+<style scoped>
+
+</style>
