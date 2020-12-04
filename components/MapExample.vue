e0f96f76113319c1ffe4b4802dcf27b94d376b9f
kaspar suursalu <kaspar.suursalu@tptlive.ee>
2020-12-04 14:18:20 +0200

    geojson map

--- components/MapExample.vue
+++ components/MapExample.vue
@@ -1,9 +1,15 @@
 <template>
-<div id="map"></div>
+  <div>
+    <button class="button is-primary" @click="addMarker">Add Marker</button>
+    <div id="map" ref="map"></div>
+
+  </div>
+
 </template>
 
 <script>
 import { Loader } from "@googlemaps/js-api-loader";
+import axios from 'axios';
 export default {
 name: "MapExample",
   mounted() {
@@ -12,22 +18,60 @@ name: "MapExample",
       version: "weekly",
     });
     loader.load().then(() => {
-      let map = new google.maps.Map(document.getElementById("map"), {
+      this.map = new google.maps.Map(this.$refs['map'], {
         center: { lat: 20, lng: 0 },
         zoom: 3,
       });
+      //this.map.data.loadGeoJson('https://raw.githubusercontent.com/johan/world.geo.json/master/countries.geo.json')
+      axios.get('https://raw.githubusercontent.com/johan/world.geo.json/master/countries.geo.json').then(resp => {
+        axios.get('https://api.covid19api.com/summary').then(covidResp => {
+            let newfeatures = Object.assign({}, resp.data);
+            newfeatures.features = resp.data.features.map(feature => {
+              let country = covidResp.data.Countries.find(country => country.Country === feature.properties.name);
+              feature.properties.cases = country ? country.TotalConfirmed : 0;
+              return feature;
+            });
+            console.log(newfeatures);
+            this.map.data.addGeoJson(newfeatures);
+            this.map.data.setStyle((feature)=> {
+              let color;
+              if(feature.getProperty('cases') >= 1){
+                color = 'green';
+              }
+              if(feature.getProperty('cases') >= 10000){
+                color = 'yellow'
+              }
+              if(feature.getProperty('cases') >= 100000){
+                color = 'red'
+              }
+              return {
+                fillColor: color
+              }
+            })
+        })
+
+      })
     });
+
+  },
+  data(){
+    return {
+      map: null
+    }
+  },
+  methods: {
+    addMarker(){
+      let marker = new google.maps.Marker({
+        position: {lat:59.4269493,lng:24.7434494},
+        map: this.map
+      });
+    }
   }
 }
 </script>
 
 <style>
-#map, #app, .section, #app>div {
-  height: 100%;
-}
-html, body {
-  height: 100%;
-  margin: 0;
-  padding: 0;
+#map {
+  height: 1000px;
 }
 </style>
